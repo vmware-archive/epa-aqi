5eb0ed602800eb47f0d76f27acd02364d24db9f5
Genevieve Lesperance <glesperance@pivotal.io>
2015-12-23 10:44:48 -0800

    [#110533460] Simplify calculator test
    Signed-off-by: John Ryan <jryan@pivotal.io>

--- spec/domain/air_quality_grade_calculator_spec.rb
+++ spec/domain/air_quality_grade_calculator_spec.rb
@@ -1,13 +1,14 @@
+def generate_measure_of_category(number, name)
+  "{
+    \"Category\": {
+      \"Number\": #{number},
+      \"Name\": \"#{name}\"
+    }
+  }"
+end
+
 def generate_measures_of_category(number, name)
-  JSON.parse("
-        [
-            {
-                \"Category\": {
-                    \"Number\": #{number},
-                    \"Name\": \"#{name}\"
-                }
-            }
-        ]")
+  JSON.parse('[' + generate_measure_of_category(number, name)+']')
 end
 
 describe AirQualityGradeCalculator do
@@ -16,66 +17,60 @@ describe AirQualityGradeCalculator do
   let(:expected_measures) { generate_measures_of_category(1, 'Good') }
   let(:zipcode) { '90210' }
 
+  subject do
+    aqg_calculator = AirQualityGradeCalculator.new
+    aqg_calculator.epa_service(mock_epa_service)
+    aqg_calculator.for_zipcode(zipcode)
+  end
+
   before do
     allow(mock_epa_service).to receive(:get_measures_for_zipcode).and_return(expected_measures)
-    subject.epa_service(mock_epa_service)
   end
 
   describe '#for_zipcode' do
     it 'asks the EPA Service for the AQI value for the zipcode supplied' do
-      subject.for_zipcode(zipcode)
+      subject
       expect(mock_epa_service).to have_received(:get_measures_for_zipcode).with(zipcode)
     end
 
     describe 'the least healthy measure is a category 1 ("Good")' do
       let(:expected_measures) { generate_measures_of_category(1, 'Good') }
-      it('returns a grade of "A"') { expect(subject.for_zipcode(zipcode)).to eq 'A' }
+      specify { expect(subject).to eq 'A' }
     end
 
     describe 'the least healthy measure is a category 2 ("Moderate")' do
       let(:expected_measures) { generate_measures_of_category(2, 'Moderate') }
-      it('returns a grade of "B"') { expect(subject.for_zipcode(zipcode)).to eq 'B' }
+      specify { expect(subject).to eq 'B' }
     end
 
     describe 'the least healthy measure is a category 3 ("Unhealthy for Sensitive")' do
       let(:expected_measures) { generate_measures_of_category(3, 'Unhealthy for Sensitive') }
-      it('returns a grade of "C"') { expect(subject.for_zipcode(zipcode)).to eq 'C' }
+      specify { expect(subject).to eq 'C' }
     end
 
     describe 'the least healthy measure is a category 4 ("Unhealthy")' do
       let(:expected_measures) { generate_measures_of_category(4, 'Unhealthy') }
-      it('returns a grade of "D"') { expect(subject.for_zipcode(zipcode)).to eq 'D' }
+      specify { expect(subject).to eq 'D' }
     end
 
     describe 'the least healthy measure is a category 5 ("Very Unhealthy")' do
       let(:expected_measures) { generate_measures_of_category(5, 'Very Unhealthy') }
-      it('returns a grade of "E"') { expect(subject.for_zipcode(zipcode)).to eq 'E' }
+      specify { expect(subject).to eq 'E' }
     end
 
     describe 'the least healthy measure is a category 6 ("Hazardous")' do
       let(:expected_measures) { generate_measures_of_category(6, 'Hazardous') }
-      it('returns a grade of "F"') { expect(subject.for_zipcode(zipcode)).to eq 'F' }
+      specify { expect(subject).to eq 'F' }
     end
 
     describe 'when the least healthy measure is not the first' do
       let(:expected_measures) {
-        JSON.parse("
-        [
-            {
-                \"Category\": {
-                    \"Number\": 1,
-                    \"Name\": \"Good\"
-                }
-            },
-            {
-                \"Category\": {
-                    \"Number\": 6,
-                    \"Name\": \"Hazardous\"
-                }
-            }
-        ]")
+        JSON.parse('[' +
+                       generate_measure_of_category(1, 'Good') + ', ' +
+                       generate_measure_of_category(6, 'Hazardous') +
+                   ']');
       }
-      it('returns the least healthy measure') { expect(subject.for_zipcode(zipcode)).to eq 'F'}
+      specify { expect(subject).to eq 'F'}
     end
   end
 end
