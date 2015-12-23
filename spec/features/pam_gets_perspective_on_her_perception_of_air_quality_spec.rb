5984366b4991ad57d6bfac11dc1bae37c17c0896
Genevieve Lesperance <glesperance@pivotal.io>
2015-12-23 12:06:22 -0800

    [#110533562] Pam can grade air quality by letter
    Signed-off-by: John Ryan <jryan@pivotal.io>

---
+++ spec/features/pam_gets_perspective_on_her_perception_of_air_quality_spec.rb
@@ -0,0 +1,42 @@
+class User
+  include ::RSpec::Matchers
+  include Capybara::DSL
+
+  def goes_to_home_page
+    visit '/'
+    expect(find('#page-title')).to have_text('Home Page')
+  end
+
+  def enters_her_zipcode
+    expect(page).to have_field('zipcode')
+    fill_in 'zipcode', with: 90401
+  end
+
+  def rates_local_air_quality
+    expect(page).to have_content('What does the air feel like today?')
+    choose('C')
+  end
+
+  def clicks_see_what_the_epa_says
+    click_on 'Submit'
+    expect(find('#page-title')).to have_text('City Detail')
+  end
+
+  def sees_the_aqi_grade
+    expect(page).to have_css('#aqi-grade')
+    expect(find('#aqi-grade')).to have_content(/[A-F]/)
+  end
+end
+
+
+describe 'Pam gets perspective', :type => :feature do
+  let(:pam) { User.new }
+  it 'by specifying her location and seeing what the actual air quality is' do
+    pam.goes_to_home_page
+    pam.enters_her_zipcode
+    pam.rates_local_air_quality
+    pam.clicks_see_what_the_epa_says
+
+    pam.sees_the_aqi_grade
+  end
+end
