d392974bcb51ba6ff97e718730dc0d1d4aad2b90
Genevieve Lesperance <glesperance@pivotal.io>
2015-12-24 16:15:06 -0800

    [#110671858] Adjust widths to match visual design
    Signed-off-by: John Ryan <jryan@pivotal.io>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,11 @@ module ApplicationHelper
       one_pin: 'single-pin',
       two_pin: 'multiple-pins'
   }
+  SLOT_WIDTHS = [10, 10, 10, 10, 20, 40]
+
+  def slot_widths
+    SLOT_WIDTHS
+  end
 
   def slot_type_to_css_class(slot_type)
     TYPE_TO_CLASS[slot_type] || 'empty-slot'
